9f227e3a9edce79d5fe27e38552c77918d4ade45
AlejandroHPi <alejandrohpisabarro@gmail.com>
2017-10-25 08:51:20 +0200

    Añadir fecha de modificacion

--- index.php
+++ index.php
@@ -4,12 +4,28 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <div>Hola developer</div>
-    </body>
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		
+		<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" >
+		<meta name="generator" content="Notepad ++" >
+		<meta name="keywords" content="" >
+		<meta name="author" content="Alejandro">
+		<meta name="description" content="" >
+		<meta name="robots" content="noindex">
+		<meta name="robots" content="nofollow">
+		<link rel="shortcut icon" href="images/favicon.jpg" type="image/x-icon" />
+		<link rel="stylesheet" type="text/css" href="index.css" />
+		<title>Indice Alejandro Hernandez Pisabarro</title>
+	</head>
+	<body>
+		<h1>Alejandro Hernandez Pisabarro</h1>
+		<ul>
+			<li><a href="DWES/DWES.html">DESARROLLO WEB EN ENTORNO SERVIDOR</a></li>
+			<li><a href="#">DESARROLLO WEB EN ENTORNO CLIENTE </a></li>
+			<li><a href="#">DISENO DE INTERFACES</a></li>
+			
+		</ul>
+	</body>
 </html>
